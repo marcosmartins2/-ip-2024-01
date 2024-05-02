525b2bc9642aba28db939fcf6a2b5270cda6ff56
marcosmartins2 <164579513+marcosmartins2@users.noreply.github.com>
2024-05-02 20:37:16 -0300

    Update exercicio10.go

--- LISTA03-GO/exercicio10.go
+++ LISTA03-GO/exercicio10.go
@@ -1,1 +1,44 @@
+package main
+
+import "fmt"
+
+var n int
+
+func main() {
+	cont := 0
+	vetor2 := 0
+	i2 := 0
+
+	fmt.Scanln(&n)
+
+	vetor := make([]int, n)
+
+	for i := 0; i < n; i++ {
+
+		fmt.Scanln(&vetor[i])
+
+	}
+	for j := 0; j < n; j++ {
+
+		if vetor[j] == vetor[n-1] {
+
+			cont++
+		}
+
+	}
+	vetor2 = 0
+	for k := 0; k < n; k++ {
+
+		if vetor2 < vetor[k] {
+
+			vetor2 = vetor[k]
+			i2 = k
+
+		}
+
+	}
+
+	fmt.Printf("NOTA %v, %v vezes\n NOTA %v, índice %v", vetor[n-1], cont, vetor2, i2)
+
+}
 
