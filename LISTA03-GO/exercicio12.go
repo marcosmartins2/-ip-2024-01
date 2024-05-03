af2dcf3e0046d5bfc0d4dc050eb6f808fa3d1c83
marcosmartins2 <164579513+marcosmartins2@users.noreply.github.com>
2024-05-03 14:24:21 -0300

    Create exercicio12.go

---
+++ LISTA03-GO/exercicio12.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var n int
+
+func main() {
+
+	fmt.Scanln(&n)
+
+	vetor := make([]int, n)
+
+	for i := 0; i < n; i++ {
+
+		fmt.Scanln(&vetor[i])
+
+	}
+	sort.Ints(vetor)
+
+	fmt.Println(vetor)
+
+}
