0d361ff83a54c4b175c6a7e7f2799bdff7485be2
marcosmartins2 <164579513+marcosmartins2@users.noreply.github.com>
2024-05-10 20:15:21 -0300

    Update exercicio13.go

--- LISTA03-GO/exercicio13.go
+++ LISTA03-GO/exercicio13.go
@@ -1,1 +1,46 @@
+package main 
 
+
+import(
+    "fmt"
+    "sort"
+    )
+
+var n,cont,numero int
+
+func main(){
+    var cont2 = 0
+    
+    fmt.Scanln(&n)
+    
+    slice := make([]int,n)
+    
+    
+    for i:=0;i<n;i++{
+        
+        fmt.Scanln(&slice[i])
+        
+        
+        
+        
+    }
+    sort.Ints(slice)
+    numero := slice[n-1]
+    
+    for j:=0;j<n;j++{
+        cont = 0
+        
+        for k:=0;k<n;k++{
+            
+            if slice[j] == slice[k]{
+                cont++
+            }
+            if cont>cont2  {
+                cont2 = cont
+                numero = slice[j]
+            }
+        }
+        
+    }
+     fmt.Printf("%v \n%v",numero,cont2)
+}
