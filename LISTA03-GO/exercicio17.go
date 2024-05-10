1aadb46e513a1aad8854d51c84492abe24568fba
marcosmartins2 <164579513+marcosmartins2@users.noreply.github.com>
2024-05-10 20:36:34 -0300

    Update exercicio17.go

--- LISTA03-GO/exercicio17.go
+++ LISTA03-GO/exercicio17.go
@@ -1,1 +1,34 @@
+package main 
 
+
+import"fmt"
+    
+var n,cont int
+
+
+
+func main(){
+    
+    fmt.Scanln(&n)
+    
+    slice := make([]int,n)
+    
+    
+    for i:=0;i<n;i++{
+        
+        
+        fmt.Scan(&slice[i])
+    }
+    
+    frequencia := make(map[int]int)
+    for _, num := range slice {
+        frequencia[num]++
+    }
+    for i:=0;i<n;i++{
+        if frequencia[i] == 1{
+            cont++
+        }
+    }
+    fmt.Println(cont+1)
+    }
+    
